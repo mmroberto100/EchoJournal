14ad3d5050a2632a71a14f2af69d3a012809e68a
manuelrobertomunoz <mmroberto100@gmail.com>
2025-01-18 02:31:09 -0700

    Added Material/Colors Schemes

--- app/src/main/java/com/mmunoz/echojournal/ui/theme/Theme.kt
+++ app/src/main/java/com/mmunoz/echojournal/ui/theme/Theme.kt
@@ -9,45 +9,42 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    surface = Surface,
+    surfaceVariant = Surface_Variant,
+    inverseOnSurface = Inverse_on_Surface,
+    onSurface = On_Surface,
+    onSurfaceVariant = On_Surface_Variant,
+    outline = Outline,
+    outlineVariant = Outline_Variant,
+    primary = Primary,
+    primaryContainer = Primary_Container,
+    onPrimary = On_Primary,
+    inversePrimary = Inverse_Primary,
+    secondary = Secondary,
+    secondaryContainer = Secondary_Container,
+    background = Background,
+    onErrorContainer = On_Error_Container,
+    errorContainer = Error_Container,
+    onError = On_Error
 )
 
 @Composable
 fun EchoJournalTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    val colorScheme = DarkColorScheme
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
     }
 
     MaterialTheme(
